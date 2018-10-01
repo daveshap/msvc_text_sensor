fcc80170508d65104e2a767618f6d6f00d8b9bd3
David Shapiro <daveshap37@gmail.com>
2018-10-01 18:28:47 -0400

    should work

--- demo_client.py
+++ demo_client.py
@@ -4,6 +4,6 @@ import json
 
 if __name__ == '__main__':
     while True:
-        messag3 = input("Enter a test message:")
-        response = requests.request(method='POST', url='127.0.0.1:6001/text', json={'message': "hey how's it going?"})
+        message = input("Enter a test message:")
+        response = requests.request(method='POST', url='http://127.0.0.1:6001/text', json={'message': message})
         print(response.text)
--- text_sensor.py
+++ text_sensor.py
@@ -1,10 +1,32 @@
+"""
+SUMMARY:
+    allows maragi to receive chat messages
+
+INPUT:
+    text message via REST API (flask)
+
+OUTPUT:
+    standard sensor message
+
+OUTPUT EXAMPLE:
+    {data: <sentence>,
+    uuid: <uuid>,
+    time: <unix>,
+    date: <datetime>,
+    sensor_name: text_chat,
+    sensor_type: API,
+    sensor_data: string}
+"""
+
+import time
+import datetime
+import uuid
 import json
 import flask
 import pika
 
 
 app_port = 6001
-app_uri = '/text'
 app = flask.Flask(__name__)
 
 
@@ -16,17 +38,29 @@ def amqp_connect():
     return connection, channel
 
 
-@app.route(app_uri, methods=['POST'])
+def publish_amqp(message):
+    con, chan = amqp_connect()
+    chan.basic_publish(exchange='sensor_text_api', body=message, routing_key='')
+    chan.close()
+    con.close()
+
+
+@app.route('/text', methods=['POST'])
 def default():
     request = flask.request
     payload = json.loads(request.data)
     print(payload)
     if request.method == 'POST':
-        con, chan = amqp_connect()
-        chan.basic_publish(exchange='sensor_text_chat', body=payload, routing_key='')
-        chan.close()
-        con.close()
-        return json.dump({'result': 'got it!'})
+        message = {'data': payload['message'],
+                   'time': str(time.time()),
+                   'uuid': str(uuid.uuid4()),
+                   'datetime': str(datetime.datetime.now()),
+                   'sensor_name': 'text_sensor',
+                   'sensor_type': 'API',
+                   'sensor_data': 'string'}
+        text = json.dumps(message)
+        publish_amqp(text)
+        return text
 
 
 if __name__ == "__main__":
